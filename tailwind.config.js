2b22675c421d85cac0cd09adc154d230fc6349e1
Linsong Zhan <happyzhanls@gmail.com>
2025-03-24 23:02:59 +0800

    refactor: remove preflight core plugin from Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  corePlugins: {
-    preflight: false,
-  },
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,json}'],
   important: '#root',
   theme: {
